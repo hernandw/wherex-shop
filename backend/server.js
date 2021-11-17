5591228221f0d516c262465f962e7f8e6404afd4
hernandw <hernandw@gmail.com>
2021-11-17 14:02:06 -0300

    configurar reduc details product

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,20 @@ app.listen(port, (req, res)=>{
     console.log(`Servidor activo en port ${port}`);
 });
 
-app.get('/', (req, res)=>{
-    res.send('Servidor Activo');
-})
+
+app.get('/api/products/:id', (req, res) =>{
+const product = data.products.find((x) => x._id === req.params.id);
+if(product){
+   res.send(product); 
+} else {
+    res.status(404).send({message: 'Product not Found'});
+}
+});
 
 app.get('/api/products', (req, res)=>{
     res.send(data.products);
+});
+
+app.get('/', (req, res)=>{
+    res.send('Servidor Activo');
 })
